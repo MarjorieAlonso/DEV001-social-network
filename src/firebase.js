00216bb57bc4e81ffe5e14714e800ee91540acce
MelissaRV <melissa.rvillegas206@gmail.com>
2022-11-21 12:07:32 -0600

    titulo

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDysxbZcvLWYEXuYDnGghgdWEeyfX_QIH8",
+    authDomain: "recomiendame-la-movie.firebaseapp.com",
+    projectId: "recomiendame-la-movie",
+    storageBucket: "recomiendame-la-movie.appspot.com",
+    messagingSenderId: "8704091286",
+    appId: "1:8704091286:web:0d3f77a39b5bd0c6e04eb7"
+  };
+
+  const app = initializeApp(firebaseConfig);
