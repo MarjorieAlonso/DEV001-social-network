82614c46449f76c4248fb465f6180d7965da1883
MelissaRV <melissa.rvillegas206@gmail.com>
2022-11-23 09:59:17 -0600

    primer template listo

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,17 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDysxbZcvLWYEXuYDnGghgdWEeyfX_QIH8",
-    authDomain: "recomiendame-la-movie.firebaseapp.com",
-    projectId: "recomiendame-la-movie",
-    storageBucket: "recomiendame-la-movie.appspot.com",
-    messagingSenderId: "8704091286",
-    appId: "1:8704091286:web:0d3f77a39b5bd0c6e04eb7"
-  };
+  apiKey: 'AIzaSyDysxbZcvLWYEXuYDnGghgdWEeyfX_QIH8',
+  authDomain: 'recomiendame-la-movie.firebaseapp.com',
+  projectId: 'recomiendame-la-movie',
+  storageBucket: 'recomiendame-la-movie.appspot.com',
+  messagingSenderId: '8704091286',
+  appId: '1:8704091286:web:0d3f77a39b5bd0c6e04eb7',
+};
 
-  const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,43 @@
-
+import './firebase.js'
 // Este es el punto de entrada de tu aplicacion
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+let showLogin = function () {
+  const templateFormulario = `
+    <div class= "container">
+      <img src="claqueta.png" class="claqueta" alt="claqueta">
+        <div class= "form">
+            <div class= "form-login">
+            <span class= title>Login</span>
+            <form action="#">
+                <div class= "input-field">
+                <input type= "text" placeholder= "Ingresa tu mail" required></input>
+                </div>
+            <div class= "input-field">
+                <input type= "text" placeholder="Ingresa tu contraseña" required></input>
+            </div>
+            <div class= "check-box-text">
+             <div class= "check-box-content">
+              <input type= "checkbox" id= "logCheck">
+              <label for= "logCheck" class="text">Recordarme</label>
+             </div>
+             <a href="#" class="text">Olvidaste tu contraseña?</a>
+            </div> 
+             <div class= "input-field-button">
+              <input type= "button" value="Entrar">
+             </div> 
+            </form>
+              <div class="login-signup">
+               <span class= "text">Aún no te registras?
+                <a href="#" class="signup-text">Registrarme</a>
+               </span>
+              </div>
+            </div>  
+        </div>
+    </div>`;
+  document.getElementById('root').innerHTML = templateFormulario;
+};
+showLogin();
 
