5cda56614649791c8e143161f44fee8eeaebfe44
MelissaRV <melissa.rvillegas206@gmail.com>
2023-01-03 17:12:40 -0600

    se muestran los comentarios

--- src/componentes/muro.js
+++ src/componentes/muro.js
@@ -1,6 +1,7 @@
 // import { addDoc, collection } from 'firebase/firestore';
 // eslint-disable-next-line no-unused-vars
 import { onSnapshot, doc, collection } from 'firebase/firestore';
+import { async } from 'regenerator-runtime';
 import { currentUserInfo } from '../lib/auth.js';
 import { saveTask, db } from '../lib/firebase.js';
 
@@ -16,22 +17,35 @@ export const showMuro = function () {
 <textarea name="mensaje" id="mensaje1" class="mensaje1" cols="50" rows="10" placeholder="Escribe aqui"></textarea>
 <button class="botonP" id= "botonP">Publicar</button>
 </form>
-<div class="botonesMuro">
-<button type="submit" class="botonE" id= "botonM">Editar</button>
-<button type="submit" class="botonEl" id= "botonM">Eliminar</button>
-</div>
-<div class="comentarios">Aqui van los comentarios
-</div>
+<div id= "container"></div>
 `;
-
+document.getElementById('root').innerHTML = templatePrincipal;
+const taskForm = document.getElementById('task-form')
+const taskContainer = document.getElementById('container')
   onSnapshot(collection(db, 'task'), querySnapshot => {
     // eslint-disable-next-line arrow-parens, no-shadow
+    let html= ''
     querySnapshot.forEach(doc => {
-      // console.log(doc.data());
+      const tasks = doc.data()
+      console.log(tasks)
+      html+= `
+      <div class= "form">
+      <div class="usuarios">
+       <h4>${tasks.userName}</h4>
+       </div>
+       <div class="comentarios">
+       <h4>${tasks.mensaje1}</h4>
+      </div>
+      <div class="botonesMuro">
+      <button type="submit" class="botonE" id= "botonM">Editar</button>
+      <button type="submit" class="botonEl" id= "botonM">Eliminar</button>
+      </div>
+      </div>
+      `
     });
+    taskContainer.innerHTML += html
   });
   // eslint-disable-next-line no-multi-assign, no-unused-vars
-  document.getElementById('root').innerHTML = templatePrincipal;
   const publish = document.getElementById('botonP');
   publish.addEventListener('click', (e) => {
     e.preventDefault();
