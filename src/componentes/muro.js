e0911005398f5978167364bd604c4d809a6354e0
ann-dy2712 <113961615+ann-dy2712@users.noreply.github.com>
2023-01-03 18:52:34 -0600

    Estilos del muro

--- src/componentes/muro.js
+++ src/componentes/muro.js
@@ -1,7 +1,6 @@
 // import { addDoc, collection } from 'firebase/firestore';
 // eslint-disable-next-line no-unused-vars
 import { onSnapshot, doc, collection } from 'firebase/firestore';
-import { async } from 'regenerator-runtime';
 import { currentUserInfo } from '../lib/auth.js';
 import { saveTask, db } from '../lib/firebase.js';
 
@@ -13,30 +12,28 @@ export const showMuro = function () {
   const templatePrincipal = `
 
 <h3> Hola ${currentUserInfo().displayName}</h3>
-<form id="task-form">
-<textarea name="mensaje" id="mensaje1" class="mensaje1" cols="50" rows="10" placeholder="Escribe aqui"></textarea>
+<form id="task-form" class="containerComents">
+<textarea name="mensaje" id="mensaje1" class="mensaje1" cols="50" rows="10" placeholder="Escribe aqui tu recomendación"></textarea>
 <button class="botonP" id= "botonP">Publicar</button>
 </form>
 <div id= "container"></div>
 `;
-document.getElementById('root').innerHTML = templatePrincipal;
-const taskForm = document.getElementById('task-form')
-const taskContainer = document.getElementById('container')
+  document.getElementById('root').innerHTML = templatePrincipal;
+  // eslint-disable-next-line no-unused-vars
+  const taskForm = document.getElementById('task-form');
+  const taskContainer = document.getElementById('container');
+  // eslint-disable-next-line arrow-parens
   onSnapshot(collection(db, 'task'), querySnapshot => {
     // eslint-disable-next-line arrow-parens, no-shadow
-    let html= ''
+    let html = '';
     querySnapshot.forEach(doc => {
-      const tasks = doc.data()
-      console.log(tasks)
-      html+= `
-      <div class= "form">
-      <div class="usuarios">
+      const tasks = doc.data();
+      console.log(tasks);
+      html += `
+      <div class= "comentarios">
        <h4>${tasks.userName}</h4>
-       </div>
-       <div class="comentarios">
-       <h4>${tasks.mensaje1}</h4>
-      </div>
-      <div class="botonesMuro">
+       <h5>${tasks.mensaje1}</h5>
+       <div class="botones">
       <button type="submit" class="botonE" id= "botonM">Editar</button>
       <button type="submit" class="botonEl" id= "botonM">Eliminar</button>
       </div>
